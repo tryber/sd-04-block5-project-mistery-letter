3de73b0f45697484f607676f8c1d62dd347a4f0d
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-09 16:12:49 -0300

    add straight class

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const criarCarta = document.getElementById('criar-carta');
 const cartaTexto = document.getElementById('carta-texto');
-let cartaGerada = document.getElementById('carta-gerada');
+const cartaGerada = document.getElementById('carta-gerada');
 const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
 const sizeGroup = ['medium', 'big', 'reallybig'];
-const rotateGroup = ['rotateleft', 'rotateright'];
-const skewGroup = ['skewleft', 'skewright'];
+const rotateGroup = ['rotateleft', 'rotateright', 'stright'];
+const skewGroup = ['skewleft', 'skewright', 'straight'];
 const groups = [styleGroup, sizeGroup, rotateGroup, skewGroup];
 
 function randomGroup(group) {
   const randomNumber = Math.floor(Math.random() * group.length);
   return group[randomNumber];
-};
+}
 
 function addClass(element, newClass) {
   element.classList.add(newClass);
-};
+}
 
 criarCarta.addEventListener('click', () => {
   cartaGerada.innerHTML = null;
   const words = cartaTexto.value.split(' ');
   for (let i = 0; i < words.length; i += 1) {
-    let word = document.createElement('span')
+    const word = document.createElement('span');
     word.innerHTML = `${words[i]}`;
-    for (let i = 0; i < groups.length; i += 1) {
-      addClass(word, randomGroup(groups[i]))
+    for (let j = 0; j < groups.length; j += 1) {
+      addClass(word, randomGroup(groups[j]));
     }
-    word
+    word;
     cartaGerada.appendChild(word);
   }
 });
-
