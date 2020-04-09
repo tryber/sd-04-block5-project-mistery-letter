3e3bece6ef108b953bd647f45805b1dc5ab614b5
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-09 15:44:38 -0300

    add random class

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const criarCarta = document.getElementById('criar-carta');
 const cartaTexto = document.getElementById('carta-texto');
 let cartaGerada = document.getElementById('carta-gerada');
-console.log(cartaTexto);
-console.log(criarCarta);
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotateGroup = ['rotateleft', 'rotateright'];
+const skewGroup = ['skewleft', 'skewright'];
+const groups = [styleGroup, sizeGroup, rotateGroup, skewGroup];
+
+function randomGroup(group) {
+    const randomNumber = Math.floor(Math.random() * group.length);
+    return group[randomNumber];
+};
+
+function addClass(element, newClass) {
+    element.classList.add(newClass);
+};
 
 criarCarta.addEventListener('click', () => {
     cartaGerada.innerHTML = null;
     const words = cartaTexto.value.split(' ');
     for (let i = 0; i < words.length; i += 1) {
         let word = document.createElement('span')
-        word.innerHTML = ` ${words[i]} `;
+        word.innerHTML = `${words[i]}`;
+        for (let i = 0; i < groups.length; i += 1) {
+            addClass(word, randomGroup(groups[i]))
+        }
+        word
         cartaGerada.appendChild(word);
     }
 });
