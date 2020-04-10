6de9246fe650c5f48cc127114686a3a9ac71ab8e
orlandomessias <programadorlando@gmail.com>
2020-04-10 19:02:25 -0300

    add new random style

--- script.js
+++ script.js
@@ -13,27 +13,37 @@ btnGenerate.addEventListener('click', () => {
     elem.appendChild(text);
     letter.appendChild(elem);
     let styles = getStylesNames();
-    applyStyles(elem, styles[0], styles[1]);
+    applyStyles(elem, styles[0], styles[1], styles[2]);
   }
 });
+
+// console.log(getStylesNames())
 
 function getStylesNames() {
   let styles = [];
   let firstIndex = Math.floor(Math.random() * 4);
   let secondIndex = firstIndex;
   while (secondIndex === firstIndex) {
-      secondIndex = Math.floor(Math.random() * 3);
+    secondIndex = Math.floor(Math.random() * 4);
+  }
+  let thirdIndex = secondIndex;
+  while (thirdIndex === firstIndex || thirdIndex === secondIndex) {
+    thirdIndex = Math.floor(Math.random() * 4);
   }
   let i = Math.floor(Math.random() * arrStyles[firstIndex].length);
   let j = Math.floor(Math.random() * arrStyles[secondIndex].length);
+  let k = Math.floor(Math.random() * arrStyles[thirdIndex].length);
   let style1 = arrStyles[firstIndex][i];
   let style2 = arrStyles[secondIndex][j];
+  let style3 = arrStyles[thirdIndex][k];
   styles[0] = style1;
   styles[1] = style2;
+  styles[2] = style3;
   return styles;
 }
 
-function applyStyles(elem, style1, style2) {
+function applyStyles(elem, style1, style2, style3) {
   elem.classList.add(style1);
   elem.classList.add(style2);
+  elem.classList.add(style3);
 }
