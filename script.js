feb165f85e943f2d5c5e6ef00a4b616f5fd80001
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-12 17:22:03 -0300

    Retiradas funções complementares.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const cartaTexto = document.getElementById('carta-texto'); // Manipula a caixa d
 const btnCriarCarta = document.getElementById('criar-carta'); // Manipula botão gerar carta.
 const cartaGerada = document.getElementById('carta-gerada'); // Manipula cartas geradas.
 
-function insereCarta(vet) {
-  cartaGerada.innerHTML = '';
-  for (let i = 0; i < vet.length; i += 1) {
-    const carta = document.createElement('span');
-    carta.innerText = vet[i];
-    cartaGerada.appendChild(carta);
-  }
-}
+//function insereCarta(vet) {
+//  cartaGerada.innerHTML = '';
+//  for (let i = 0; i < vet.length; i += 1) {
+//    const carta = document.createElement('span');
+//    carta.innerText = vet[i];
+//    cartaGerada.appendChild(carta);
+//  }
+//}
 
 //function geraCarta() {
 //  if (cartaTexto.value === '') {
@@ -22,7 +22,12 @@ function insereCarta(vet) {
 
 function geraCarta() {
   const textoDividido = cartaTexto.value.split(' '); // Vetor onde cada palavra é um índice.
-  insereCarta(textoDividido);
+  cartaGerada.innerHTML = '';
+  for (let i = 0; i < textoDividido.length; i += 1) {
+    const carta = document.createElement('span');
+    carta.innerText = textoDividido[i];
+    cartaGerada.appendChild(carta);
+  }
 }
 
 function eventBtnCriarCarta() {
