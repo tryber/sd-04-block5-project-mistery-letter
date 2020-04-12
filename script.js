7957d29b8184c85a78ec7604b7a48a605fb55548
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-12 19:28:07 -0300

    Criada a class medium.

--- script.js
+++ script.js
@@ -2,31 +2,20 @@ const cartaTexto = document.getElementById('carta-texto'); // Manipula a caixa d
 const btnCriarCarta = document.getElementById('criar-carta'); // Manipula botão gerar carta.
 const cartaGerada = document.getElementById('carta-gerada'); // Manipula cartas geradas.
 
-//function insereCarta(vet) {
-//  cartaGerada.innerHTML = '';
-//  for (let i = 0; i < vet.length; i += 1) {
-//    const carta = document.createElement('span');
-//    carta.innerText = vet[i];
-//    cartaGerada.appendChild(carta);
-//  }
-//}
-
-//function geraCarta() {
-//  if (cartaTexto.value === '') {
-//    alert('Não há texto para gerar cartas.');
-//  } else {
-//    const textoDividido = cartaTexto.value.split(' '); // Vetor onde cada palavra é um índice.
-//    insereCarta(textoDividido);
-//  }
-//}
+function insereCarta(vet) {
+  for (let i = 0; i < vet.length; i += 1) {
+    const carta = document.createElement('span');
+    carta.innerText = vet[i];
+    cartaGerada.appendChild(carta);
+  }
+}
 
 function geraCarta() {
-  const textoDividido = cartaTexto.value.split(' '); // Vetor onde cada palavra é um índice.
-  cartaGerada.innerHTML = '';
-  for (let i = 0; i < textoDividido.length; i += 1) {
-    const carta = document.createElement('span');
-    carta.innerText = textoDividido[i];
-    cartaGerada.appendChild(carta);
+  if (cartaTexto.value === '') {
+    alert('Não há texto para gerar cartas.');
+  } else {
+    const textoDividido = cartaTexto.value.split(' '); // Vetor onde cada palavra é um índice.
+    insereCarta(textoDividido);
   }
 }
 
