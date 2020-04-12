88ecf1371d207c5086ed6e458909c73bc9f1bf48
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-12 16:23:46 -0300

    Criada funcionalidade para gerar cartas dentro do parágrafo id='carta-gerada'

---
+++ script.js
@@ -0,0 +1,28 @@
+const cartaTexto = document.getElementById('carta-texto'); // Manipula a caixa de texto.
+const btnCriarCarta = document.getElementById('criar-carta'); // Manipula botão gerar carta.
+const cartaGerada = document.getElementById('carta-gerada')
+
+function insereCarta(vet) {
+  for (let i = 0; i < vet.length; i += 1) {
+    const carta = document.createElement('span');
+    carta.innerText = vet[i];
+    cartaGerada.appendChild(carta);
+  }
+}
+
+function geraCarta() {
+  if (cartaTexto.value === '') {
+    alert('Não há texto para gerar cartas.');
+  } else {
+    const textoDividido = cartaTexto.value.split(' '); // Vetor onde cada palavra é um índice.
+    insereCarta(textoDividido);
+  }
+}
+
+function eventBtnCriarCarta() {
+  btnCriarCarta.addEventListener('click', geraCarta);
+}
+
+window.onload = function () {
+  eventBtnCriarCarta();
+};
