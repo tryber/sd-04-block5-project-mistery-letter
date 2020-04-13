50c707cb7e014a180bb6edd21abf8755ed5e4d43
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-13 16:25:36 -0300

    Add function to sort word style classes

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ makeLetterBtn.addEventListener('click', () => {
 
 function randomNumber(maxNumber) {
   const number = Math.round(Math.random() * maxNumber)
+
+  return number;
+}
+
+function  sortClass() {
+  const styleTypes = ['newspaper', 'magazine1', 'magazine2'];
+  const sizeTypes = ['medium', 'big', 'reallybig'];
+  const rotationTypes = ['rotateleft', 'rotateright'];
+  const inclinationTypes = ['skewleft', 'skewright'];
+
+  const wordStyle = {
+    style: styleTypes[randomNumber(2)],
+    size: sizeTypes[randomNumber(2)],
+    rotation: rotationTypes[randomNumber(1)],
+    inclination: inclinationTypes[(randomNumber(1))],
+  }
+
+  return wordStyle;
 }
