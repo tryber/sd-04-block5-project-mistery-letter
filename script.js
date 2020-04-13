d99d01ae1ab96b97e77bd13b0703e5e3e4cf7c91
ederusena <eder.sena@live.com>
2020-04-13 18:51:08 -0300

    SAVEPOINT

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const criarCarta = document.getElementById('criar-carta');
-const cartaTexto = document.getElementById('carta-texto');
-const cartaGerada = document.getElementById('carta-gerada');
+const criarCarta = document.getElementById("criar-carta");
+const cartaTexto = document.getElementById("carta-texto");
+const cartaGerada = document.getElementById("carta-gerada");
 
-criarCarta.addEventListener('click', () => {
-    if (cartaTexto.value) {
-        const spanElement = document.createElement('span');
-        const spanTextNode = document.createTextNode(cartaTexto.value);
-        spanElement.appendChild(spanTextNode);
-        spanElement.setAttribute('id', 'span-nodes');
-        cartaGerada.appendChild(spanElement);
+criarCarta.addEventListener("click", () => {
+  if (cartaTexto.value) {
+    const splitedText = cartaTexto.value.split(" ");
+    for (const index in splitedText) {
+      const spanElement = document.createElement("span");
+      const spanTextNode = document.createTextNode(`${splitedText[index]} `);
+      spanElement.appendChild(spanTextNode);
+      spanElement.setAttribute("id", `span-node${index}`);
+      cartaGerada.appendChild(spanElement);
     }
-})
+  }
+});
