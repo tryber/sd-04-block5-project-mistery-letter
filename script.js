77475c801945a2b1b306a95d63ff857bc700b580
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-13 16:41:31 -0300

    Add function to generate the words on letter

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+const letter = document.querySelector('#carta-gerada');
 const makeLetterBtn = document.querySelector('#criar-carta');
 makeLetterBtn.addEventListener('click', () => {
-  
+  const textInput = document.querySelector('#carta-texto').value;
+  wordsArray = explodePhrase(textInput);
+  wordsArray.forEach((word) => {
+    const wordContainer = document.createElement('span');
+    const wordStyle = sortClass();
+    for (const style in wordStyle) {
+      wordContainer.classList.add(wordStyle[style]);
+    }
+    wordContainer.innerHTML = word;
+    letter.appendChild(wordContainer);
+  });
 });
 
 function randomNumber(maxNumber) {
