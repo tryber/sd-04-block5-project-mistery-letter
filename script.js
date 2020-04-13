1a5b12035992fbef70162790f3a200c493a5da1a
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-12 21:29:04 -0300

    Criada função que adiciona classes aleotoriamente.

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const cartaTexto = document.getElementById('carta-texto'); // Manipula a caixa de texto.
 const btnCriarCarta = document.getElementById('criar-carta'); // Manipula botão gerar carta.
 const cartaGerada = document.getElementById('carta-gerada'); // Manipula cartas geradas.
+const estilos = [
+  ['newspaper', 'magazine1', 'magazine2'],
+  ['medium', 'big', 'reallybig'],
+  ['rotateleft', 'rotateright'],
+  ['skewleft', 'skewright'],
+];
+
+function adicionaClass(elemento) {
+  for (let i = 0; i < 3; i += 1) {
+    const grupo = Number.parseInt(Math.floor(Math.random() * estilos.length), 10);
+    const estilo = Number.parseInt(Math.floor(Math.random() * estilos[grupo].length), 10);
+    elemento.classList.add(estilos[grupo][estilo]);
+    if (elemento.classList.length === 3) {
+      break;
+    }
+  }
+}
 
 function insereCarta(vet) {
-  cartaGerada.innerHTML = null;
+  cartaGerada.innerHTML = '';
   for (let i = 0; i < vet.length; i += 1) {
     const carta = document.createElement('span');
     carta.innerHTML = vet[i];
+    adicionaClass(carta);
     cartaGerada.appendChild(carta);
   }
 }
