13f9cbde96184d9bf4ecf0d0f2701115f0c33a8d
ederusena <eder.sena@live.com>
2020-04-13 19:36:20 -0300

    SAVEPOINT

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const criarCarta = document.getElementById("criar-carta");
 const cartaTexto = document.getElementById("carta-texto");
 const cartaGerada = document.getElementById("carta-gerada");
+const estilo = new Array('newspaper', 'magazine1', 'magazine2');
+const tamanho = new Array('medium', 'big', 'reallybig');
+const rotacao = new Array('rotateleft', 'rotateright');
+const inclinacao = new Array('skewleft', 'skewright');
+const spanList = document.getElementsByTagName('span');
+
+
+
 
 criarCarta.addEventListener("click", () => {
-  const estilo = [`newspaper`, `magazine1`, `magazine2`];
-  const tamanho = [`medium`, `big`, `reallybig`];
-  const rotacao = [`rotateleft`, `rotateright`];
-  const inclinacao = [`skewleft`, `skewright`];
+  while (spanList.length > 0) {
+    spanList[0].remove();
+  }
 
   if (cartaTexto.value) {
     const splitedText = cartaTexto.value.split(" ");
     for (const index in splitedText) {
+      const estiloR = estilo[Math.floor(Math.random() * estilo.length)];
+      const tamanhoR = tamanho[Math.floor(Math.random() * tamanho.length)];
+      const rotacaoR = rotacao[Math.floor(Math.random() * rotacao.length)];
+      const inclinacaoR = inclinacao[Math.floor(Math.random() * inclinacao.length)];
       const spanElement = document.createElement("span");
       const spanTextNode = document.createTextNode(`${splitedText[index]}`);
 
       spanElement.appendChild(spanTextNode);
       spanElement.setAttribute("id", `carta-gerada`);
+      spanElement.setAttribute("class", `${estiloR} ${tamanhoR} ${rotacaoR} ${inclinacaoR}`);
       cartaGerada.appendChild(spanElement);
     }
   }
