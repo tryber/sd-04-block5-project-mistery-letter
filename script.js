41c1d3426fa10373d95f8a3b4acc7c24db934a9c
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-13 16:06:20 -0300

    Add criar-carta button event listener

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const makeLetterBtn = document.querySelector('#criar-carta');
+makeLetterBtn.addEventListener('click', () => {
+  
+});
+
 function randomNumber(maxNumber) {
   const number = Math.round(Math.random() * maxNumber)
 }
