91890a8072fa04fca6ecdd5e1274025f1c5032ca
Leandro Marten <leandromarten1@gmail.com>
2020-04-14 15:31:10 -0300

    testing 1 js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 const button = document.querySelector('#criar-carta');
 const input = document.querySelector('#carta-texto');
 
+// Necessary áreas
+
+
+
 // arrays groups
 
 let styleGroup = ['newspaper', 'magazine1', 'magazine2'];
@@ -10,3 +14,21 @@ let sizeGroup = ['medium', 'big', 'reallybig'];
 let rotateGroup = ['rotateleft', 'rotateright'];
 let inclineGroup = ['skewleft', 'skewright'];
 
+// create <span> tag
+
+const spanTag = document.createElement('span');
+
+// start testing area
+
+button.addEventListener('click', function(){
+  let text = input.value;
+  let pTag = document.querySelector('#texto');
+  if(input.value !== ""){
+    pTag.appendChild(spanTag);
+    pTag.firstChild.innerHTML = text;
+  }else{
+    alert('Por favor preencha o campo com o texto desejado!');
+  }
+})
+
+// end testing area
