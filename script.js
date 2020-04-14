0e8f89b150b824b341183bff986df922a39d409f
Leandro Marten <leandromarten1@gmail.com>
2020-04-14 15:11:17 -0300

    const an variables JS

---
+++ script.js
@@ -0,0 +1,12 @@
+// Button and Input
+
+const button = document.querySelector('#criar-carta');
+const input = document.querySelector('#carta-texto');
+
+// arrays groups
+
+let styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+let sizeGroup = ['medium', 'big', 'reallybig'];
+let rotateGroup = ['rotateleft', 'rotateright'];
+let inclineGroup = ['skewleft', 'skewright'];
+
