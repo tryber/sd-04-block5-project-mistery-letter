cc94e50f6c2086dacc2a0ba44ed059d3e8e058ef
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-14 17:27:34 -0300

    code climate fixes

--- script.js
+++ script.js
@@ -2,45 +2,45 @@ let resultado;
 let input;
 let count;
 
-let allClasses = {
+const allClasses = {
   0: ['newspaper', 'magazine1', 'magazine2'],
   1: ['medium', 'big', 'reallybig'],
   2: ['rotateleft', 'rotateright'],
   3: ['skewleft', 'skewright'],
-}
+};
 
 function randomClass(element) {
-  element.className = "";
-  for (let i in allClasses) {
-    let randomIndex = parseInt(Math.random() * allClasses[i].length);
+  element.className = '';
+  for (let i = 0; i < Object.keys(allClasses).length; i += 1) {
+    const randomIndex = parseInt(Math.random() * allClasses[i].length, 1);
     element.classList.add(allClasses[i][randomIndex]);
   }
 }
 
 function criarPalavra(text) {
-  let palavra = document.createElement('span');
+  const palavra = document.createElement('span');
   palavra.innerText = `${text} `;
-  palavra.addEventListener("click", function (event) {
-    randomClass(event.target)
+  palavra.addEventListener('click', function (event) {
+    randomClass(event.target);
   });
   return palavra;
 }
 
 function deletarCarta() {
-  let resultLength = resultado.childNodes.length;
+  const resultLength = resultado.childNodes.length;
   for (let i = 0; i < resultLength; i += 1) {
     resultado.removeChild(resultado.lastChild);
   }
 }
 
 function criarCarta() {
-  let words = input.value.split(' ');
+  const words = input.value.split(' ');
   count.innerText = words.length;
   if (resultado.hasChildNodes) {
     deletarCarta();
   }
   for (let i = 0; i < words.length; i += 1) {
-    let palavra = criarPalavra(words[i]);
+    const palavra = criarPalavra(words[i]);
     resultado.appendChild(palavra);
     randomClass(palavra);
   }
@@ -53,7 +53,7 @@ function addEvents() {
 function setParameters() {
   resultado = document.getElementById('carta-gerada');
   input = document.getElementById('carta-texto');
-  count = document.getElementById('carta-contador')
+  count = document.getElementById('carta-contador');
 }
 
 window.onload = function () {
