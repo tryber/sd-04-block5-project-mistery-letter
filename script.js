1904af53d74858c7b0a89563fc54b7aafb9e48cf
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-15 13:21:43 -0300

    stepbonus2 try9

--- script.js
+++ script.js
@@ -3,47 +3,38 @@ window.onload = function () {
   const paragrafo = document.getElementById('carta-gerada');
   const botaoCarta = document.getElementById('criar-carta');
   const inputCarta = document.getElementById('carta-texto');
-  const estilos = ["newspaper", "magazine1", "magazine2"];
-  const tamanhos = ["medium", "big", "reallybig"];
-  const rotação = ["rotateleft", "rotateright"];
-  const inclinação = ["skewleft", "skewright"];
-
-  let frase = '';
-  let limite = '';
+  const estilosObjeto = {
+    0: ["newspaper", "magazine1", "magazine2"],
+    1: ["medium", "big", "reallybig"],
+    2: ["rotateleft", "rotateright"],
+    3: ["skewleft", "skewright"]
+  }
 
   // eventos do botão criar carta:
   botaoCarta.addEventListener('click', () => {
+
     sortearEstilos();
     contador();
   })
 
   document.body.addEventListener('click', () => {
     frase = inputCarta.value.split(' ');
-    limite = frase.length;
     estiloClique(event);
   })
 
   // adicionando sorteio e implementação dos estilos e das frases:
   function sortearEstilos() {
     let frase = inputCarta.value.split(' ');
-
-    for (let i = 0; i < frase.length; i += 1) {
+    for (let k = 0; k < frase.length; k += 1) {
       const span1 = document.createElement('span');
-      let estiloRandom = estilos[Math.floor(Math.random() * estilos.length)];
-      span1.classList.add(estiloRandom);
-
-      let tamanhoRandom = tamanhos[Math.floor(Math.random() * tamanhos.length)];
-      span1.classList.add(tamanhoRandom);
-
-      let rotaçãoRandom = rotação[Math.floor(Math.random() * rotação.length)];
-      span1.classList.add(rotaçãoRandom);
-
-      let inclinaçãoRandom = inclinação[Math.floor(Math.random() * inclinação.length)];
-      span1.classList.add(inclinaçãoRandom);
-
-      span1.innerHTML = frase[i];
+      for (let i = 0; i < Object.keys(estilosObjeto).length; i += 1) {
+        let random = estilosObjeto[i][Math.floor(Math.random() * estilosObjeto[i].length)]
+        span1.classList.add(random);
+      }
+      span1.innerHTML = frase[k];
       paragrafo.appendChild(span1);
     }
+
   }
 
   // adicionando alteração de estilo ao clique:
@@ -51,25 +42,10 @@ window.onload = function () {
     if (event.target && event.target.nodeName == 'SPAN') {
       const target = event.target;
       target.className = '';
-      let frase = inputCarta.value.split(' ');
-
-      for (let i = 0; i < frase.length; i += 1) {
-        const span1 = document.createElement('span');
-        let estiloRandom = estilos[Math.floor(Math.random() * estilos.length)];
-        span1.classList.add(estiloRandom);
-
-        let tamanhoRandom = tamanhos[Math.floor(Math.random() * tamanhos.length)];
-        span1.classList.add(tamanhoRandom);
-
-        let rotaçãoRandom = rotação[Math.floor(Math.random() * rotação.length)];
-        span1.classList.add(rotaçãoRandom);
-
-        let inclinaçãoRandom = inclinação[Math.floor(Math.random() * inclinação.length)];
-        span1.classList.add(inclinaçãoRandom);
-
-        target.classList.add(estiloRandom, tamanhoRandom, rotaçãoRandom, inclinaçãoRandom);
+      for (let i = 0; i < Object.keys(estilosObjeto).length; i += 1) {
+        let random = estilosObjeto[i][Math.floor(Math.random() * estilosObjeto[i].length)]
+        target.classList.add(random);
       }
-
     }
   }
 
@@ -77,7 +53,7 @@ window.onload = function () {
   function contador() {
     const contador = document.getElementById('carta-contador');
     const palavrasNumero = document.querySelectorAll('span').length
-    contador.innerHTML = `Sua carta misteriosa contém ${palavrasNumero} palavras!`;
+    contador.innerHTML = palavrasNumero;
   }
 
 
