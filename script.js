5cc99d9fac95819f7b0374524f97f9c53d2560a6
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-17 13:23:33 -0300

    add class nespaper

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 window.onload = function () {
-let buttonelement = document.getElementById('criar-carta');
-buttonelement.addEventListener('click', function (){
-//console.log('click done');
-const cartaGeradElement = document.getElementById('carta-gerada');
-let textbox = document.getElementById('carta-texto').value;
-let textArray = textbox.split(" ");
-cartaGeradElement.innerText = '';
-	for (let i = 0; i < textArray.length; i += 1){
-		const span = document.createElement('span');
-		span.className = 'spanClass';
-		//console.log('textbox',textbox,'textArray :',textArray,"textArray.length :",textArray.length); //temp!
-		console.log('Hello inside de loop','i',i,'textArray[i]',textArray[i] );
-		//cartaGeradElement.appendChild(span);
-		cartaGeradElement.appendChild(span).innerHTML = textArray[i];
-	}
-})
-
-} // end of window.onload 
+// function to split the string and put it in span on button click
+  const buttonelement = document.getElementById('criar-carta');
+  buttonelement.addEventListener('click', function () {
+// console.log('click done');
+    const cartaGeradElement = document.getElementById('carta-gerada');
+    const textbox = document.getElementById('carta-texto').value;
+    const textArray = textbox.split(' ');
+    cartaGeradElement.innerText = '';
+    for (let i = 0; i < textArray.length; i += 1) {
+      const span = document.createElement('span');
+      span.className = 'spanClass';
+      cartaGeradElement.appendChild(span).innerHTML = textArray[i];
+    }
+  });
+};// end of window.onload
