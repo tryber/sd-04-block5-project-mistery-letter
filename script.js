36d5edb552c2fa6c03fd96a2804ad2bbbe75c024
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-17 19:32:28 -0300

    add sentence counter

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+ // let objectClasses = {Grupo estilo :[];}
 // function to split the string and put it in span on button click
   const buttonelement = document.getElementById('criar-carta');
   buttonelement.addEventListener('click', function () {
@@ -6,11 +7,17 @@ window.onload = function () {
     const cartaGeradElement = document.getElementById('carta-gerada');
     const textbox = document.getElementById('carta-texto').value;
     const textArray = textbox.split(' ');
-    cartaGeradElement.innerText = '';
+    while (cartaGeradElement.firstChild) {
+      cartaGeradElement.firstChild.remove();
+    }
+    const span = document.createElement('span');
     for (let i = 0; i < textArray.length; i += 1) {
       const span = document.createElement('span');
+      span.style.margin = '10px';
       //span.className = 'spanClass';
       cartaGeradElement.appendChild(span).innerHTML = textArray[i];
     }
+    const cartaContadorElemment = document.getElementById('carta-contador');
+    cartaContadorElemment.innerText = `contador de palavras: ${textArray.length}`;
   });
 };// end of window.onload
