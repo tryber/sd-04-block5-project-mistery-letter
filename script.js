703656669e7ef7d9066d3de4f579a90052bd5b41
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-18 20:47:08 -0300

    fix codeclimate

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
-window.onload = function () {
-let grupoestilo = ['newspaper', 'magazine1', 'magazine2'];
-let grupotamanho = ['reallybig', 'big', 'medium'];
-let gruporotação = ['rotateright', 'rotateleft'];
-let grupoinclinação = ['skewright', 'skewleft'];
+const grupoestilo = ['newspaper', 'magazine1', 'magazine2'];
+const grupotamanho = ['reallybig', 'big', 'medium'];
+const gruporotação = ['rotateright', 'rotateleft'];
+const grupoinclinação = ['skewright', 'skewleft'];
 function aleatoire(qda) {
   return Math.floor(Math.random() * qda);
 }
-let testaleatoire  = aleatoire(10);
-console.log('testaleatoire',testaleatoire);
+function classnameAleatoire() {
+  return `${grupotamanho[aleatoire(3)]} ${grupoestilo[aleatoire(3)]} ${gruporotação[aleatoire(2)]} ${grupoinclinação[aleatoire(2)]}`;
+}
 // function to split the string and put it in span on button click
-  const buttonelement = document.getElementById('criar-carta');
-  buttonelement.addEventListener('click', function () {
-// console.log('click done');
-    const cartaGeradElement = document.getElementById('carta-gerada');
-    const textbox = document.getElementById('carta-texto').value;
-    const textArray = textbox.split(' ');
-    while (cartaGeradElement.firstChild) {
-      cartaGeradElement.firstChild.remove();
-    }
-    for (let i = 0; i < textArray.length; i += 1) {
-      const span = document.createElement('span');
-      let classnameAleatoire = grupotamanho[aleatoire(3)] + " " + grupoestilo[aleatoire(3)] + 
-      " " + gruporotação[aleatoire(2)] + " " + grupoinclinação[aleatoire(2)];
-      span.style.margin = '10px'; // Ver se e melhor colocar no css?
-      span.className = classnameAleatoire;
-      cartaGeradElement.appendChild(span).innerHTML = textArray[i];
-    }
-    const cartaContadorElemment = document.getElementById('carta-contador');
-    // cartaContadorElemment.innerText = `contador de palavras: ${textArray.length}`;
-    cartaContadorElemment.innerText = textArray.length;
-  });
+const buttonelement = document.getElementById('criar-carta');
+buttonelement.addEventListener('click', function () {
+  const cartaGeradElement = document.getElementById('carta-gerada');
+  const textbox = document.getElementById('carta-texto').value;
+  const textArray = textbox.split(' ');
+  while (cartaGeradElement.firstChild) {
+    cartaGeradElement.firstChild.remove();
+  }
+  for (let i = 0; i < textArray.length; i += 1) {
+    const span = document.createElement('span');
+    span.style.margin = '10px'; // Ver se e melhor colocar no css?
+    span.className = classnameAleatoire();
+    cartaGeradElement.appendChild(span).innerHTML = textArray[i];
+  }
+  const cartaContadorElemment = document.getElementById('carta-contador');
+  cartaContadorElemment.innerText = textArray.length;
+});
 // //  function click on ball print a message
 document.body.addEventListener('click', function (event) {
   const tagName = event.target.tagName;
-  //console.log('tagName',tagName);
   if (tagName === 'SPAN') {
-    //console.log('Hello I am here')
-    let classnameAleatoire = grupotamanho[aleatoire(3)] + " " + grupoestilo[aleatoire(3)] + 
-      " " + gruporotação[aleatoire(2)] + " " + grupoinclinação[aleatoire(2)];
-    event.target.className = classnameAleatoire;
-    // document.querySelector('#answer').innerHTML = 'Acertou!';
-    // scorevalue += 3;
-    // scoreElement.innerHTML = `Seu score é: ${scorevalue}`;
+    event.target.className = classnameAleatoire();
   }
-  // else if (targetbackground !== `rgb${rgbColorElement.innerHTML}` && classname.includes('ball')) {
-  //   document.querySelector('#answer').innerHTML = 'Errou! Tente novamente!';
-  // }
 });
-
-};// end of window.onload
