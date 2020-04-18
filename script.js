fe4e58de0594ff33da9ff644281d4985721ff449
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-18 17:49:48 -0300

    fature/words styles randomization

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// Query selectors
 const textInput = document.querySelector('.carta-texto');
 const text = document.querySelector('.carta-gerada');
 const createBtn = document.querySelector('.criar-carta');
 
+// Add span to words
 function addSpan(str) {
   const splitText = str.split(' ');
   splitText.forEach((item) => {
     const spanElement = document.createElement('span');
-    spanElement.innerHTML = item;
+    spanElement.innerHTML = `${item} `;
     text.appendChild(spanElement);
   });
 }
 
+// Delete generated words
 function deleteText() {
   const element = document.querySelector('.carta-gerada');
   while (element.firstChild) {
@@ -18,7 +21,29 @@ function deleteText() {
   }
 }
 
+function addRandomClass() {
+  const spans = document.querySelectorAll('span');
+  spans.forEach((item) => {
+    // Word styles arrays
+    const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+    const sizeGroup = ['medium', 'big', 'reallybig'];
+    const rotationGroup = ['rotateleft', 'rotateright'];
+    const inclinationGroup = ['skewleft', 'skewright'];
+
+    const stylesArrays = [styleGroup, sizeGroup, rotationGroup, inclinationGroup];
+
+    for (let i = 1; i <= Math.round(Math.random() * (4 - 2)) + 2; i += 1) {
+      const selectedGroup = stylesArrays[Math.round(Math.random() * (stylesArrays.length - 1))];
+      const selectedStyle = selectedGroup[Math.round(Math.random() * (selectedGroup.length - 1))];
+      stylesArrays.splice(stylesArrays.indexOf(selectedGroup), 1);
+      item.classList.add(selectedStyle);
+    }
+  });
+}
+
+// Create text button
 createBtn.addEventListener('click', () => {
   deleteText();
   addSpan(textInput.value);
+  addRandomClass();
 });
