ad06659aef072a2ecf631de5d106222c1832c7c7
deboracosilveira <deboracosilveira@gmail.com>
2020-04-19 15:25:53 -0300

    requirement-15

--- script.js
+++ script.js
@@ -2,19 +2,29 @@
 const input = document.getElementById('carta-texto');
 const button = document.getElementById('criar-carta');
 const p = document.getElementById('carta-gerada');
+const classStyle = ['newspaper', 'magazine1', 'magazine2'];
+const classSize = ['medium', 'big', 'reallybig'];
+const classSlope = ['skewleft', 'skewright'];
 
-// Button click event
-button.addEventListener('click', function() {
-  p.innerHTML = '';// Clear previous generated phrase
-  createSpan();
-});
 // Creating spans from the typed phrase
 function createSpan() {
-  let words = input.value.split(' ');
+  const words = input.value.split(' ');
   for (let i = 0; i < words.length; i += 1) {
+    // Generating class names randomly
+    const style = classStyle[Math.floor(Math.random() * classStyle.length)];
+    const size = classSize[Math.floor(Math.random() * classSize.length)];
+    const slope = classSlope[Math.floor(Math.random() * classSlope.length)];
+    // Creating the span
     const span = document.createElement('span');
-    span.className = 'word';
+    span.className = style;
+    span.classList.add(size);
+    span.classList.add(slope);
     span.innerText = words[i];
     p.appendChild(span);
   }
 }
+// Button click event
+button.addEventListener('click', function () {
+  p.innerHTML = '';// Clear previous generated phrase
+  createSpan();
+});
