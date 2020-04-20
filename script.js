9089c3338ca578ad60c67bcf56c369231ad2d093
Julio Cesar <stininmatic@hotmail.com>
2020-04-20 19:48:27 -0300

    teste, começando criar as classes

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ var inputText = document.getElementById("carta-texto");
 var buttonCreate = document.querySelector("#criar-carta");
 var showPhrase = document.querySelector("#carta-gerada");
 
+var newspaper = document.createElement("div");
+newspaper.classList.add("newspaper");
+
+var magazine1 = document.createElement("div");
+magazine1.classList.add("magazine1");
+
 
 buttonCreate.addEventListener('click', mostraFrase);
 
