dfdcb6fdaea66c236f289f29fbcfd3482d21472c
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-20 19:19:12 -0300

    refactoring & codeclimate fixes

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-window.onload = function () {
+const paragrafo = document.getElementById('carta-gerada');
+const botaoCarta = document.getElementById('criar-carta');
+const inputCarta = document.getElementById('carta-texto');
+const estilosObjeto = {
+  0: ['newspaper', 'magazine1', 'magazine2'],
+  1: ['medium', 'big', 'reallybig'],
+  2: ['rotateleft', 'rotateright'],
+  3: ['skewleft', 'skewright'],
+};
 
-  const paragrafo = document.getElementById('carta-gerada');
-  const botaoCarta = document.getElementById('criar-carta');
-  const inputCarta = document.getElementById('carta-texto');
-  const estilosObjeto = {
-    0: ["newspaper", "magazine1", "magazine2"],
-    1: ["medium", "big", "reallybig"],
-    2: ["rotateleft", "rotateright"],
-    3: ["skewleft", "skewright"]
-  }
+// adicionando contador de palavras:
+function contador() {
+  const numContador = document.getElementById('carta-contador');
+  const palavrasNumero = document.querySelectorAll('span').length;
+  numContador.innerHTML = palavrasNumero;
+}
 
-  // eventos do botão criar carta:
-  botaoCarta.addEventListener('click', sortearEstilos);
-
-  document.body.addEventListener('click', () => {
-    estiloClique(event);
-  })
-
-  // adicionando sorteio e implementação dos estilos e das frases:
-  function sortearEstilos() {
-    let frase = inputCarta.value.split(' ');
-    while (paragrafo.firstChild) {
-      paragrafo.firstChild.remove();
-    }
-    for (let k = 0; k < frase.length; k += 1) {
-      if (frase[k] != '') {
-        const span1 = document.createElement('span');
-        for (let i = 0; i < Object.keys(estilosObjeto).length; i += 1) {
-          let random = estilosObjeto[i][Math.floor(Math.random() * estilosObjeto[i].length)];
-          span1.classList.add(random);
-        }
-        span1.innerHTML = frase[k];
-        paragrafo.appendChild(span1);
-      }
-    }
-
-    contador();
-  }
-
-  // adicionando alteração de estilo ao clique:
-  function estiloClique(event) {
-    if (event.target && event.target.nodeName == 'SPAN') {
-      const target = event.target;
-      target.className = '';
-      for (let i = 0; i < Object.keys(estilosObjeto).length; i += 1) {
-        let random = estilosObjeto[i][Math.floor(Math.random() * estilosObjeto[i].length)];
-        target.classList.add(random);
-      }
-    }
-  }
-
-  // adicionando contador de palavras:
-  function contador() {
-    const contador = document.getElementById('carta-contador');
-    const palavrasNumero = document.querySelectorAll('span').length;
-    contador.innerHTML = palavrasNumero;
+// limpar paragrafo:
+function limparParagrafo() {
+  while (paragrafo.firstChild) {
+    paragrafo.firstChild.remove();
   }
 }
+
+// sorteando estilos para um span:
+function sortearEstilos(spanCriado) {
+  for (let i = 0; i < Object.keys(estilosObjeto).length; i += 1) {
+    const random = estilosObjeto[i][Math.floor(Math.random() * estilosObjeto[i].length)];
+    spanCriado.classList.add(random);
+  }
+}
+// adicionando sorteio e implementando os estilos e as frases:
+function atribuirEstilos() {
+  const frase = inputCarta.value.split(' ');
+  limparParagrafo();
+  for (let k = 0; k < frase.length; k += 1) {
+    const span = document.createElement('span');
+    if (frase[k] !== '') {
+      sortearEstilos(span);
+    }
+    span.innerHTML = frase[k];
+    paragrafo.appendChild(span);
+  }
+  contador();
+}
+botaoCarta.addEventListener('click', atribuirEstilos);
+
+// adicionando alteração de estilo ao clique:
+function estiloClique(event) {
+  if (event.target && event.target.nodeName === 'SPAN') {
+    const target = event.target;
+    target.className = '';
+    for (let i = 0; i < Object.keys(estilosObjeto).length; i += 1) {
+      const random = estilosObjeto[i][Math.floor(Math.random() * estilosObjeto[i].length)];
+      target.classList.add(random);
+    }
+  }
+}
+document.body.addEventListener('click', estiloClique);
