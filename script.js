2f8d2b39e758222fb0a8890c1cadcf2f849c6204
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-20 15:42:35 -0300

    generating input valor on <p>

---
+++ script.js
@@ -0,0 +1,14 @@
+let inputFrase = document.querySelector('#carta-texto');
+let fraseResult = document.querySelector('#carta-gerada');
+let btnCriar = document.querySelector('#criar-carta');
+
+btnCriar.addEventListener('click', function() {
+    console.log(inputFrase.value);
+    mostraFrase(inputFrase.value)
+});
+
+function mostraFrase(frase) {
+   let palavra = document.createElement('span');
+   palavra.innerHTML = frase
+   fraseResult.appendChild(palavra);
+}
