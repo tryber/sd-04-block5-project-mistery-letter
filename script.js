77b415e9212689fbe0cd7d4d0e429a501bf97749
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-21 11:56:25 -0300

    Último requisito bônus

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
 const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
-const aleatory = ['newspaper', 'magazine1', 'magazine2','medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+const aleatory = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+let contador = 0;
+const countWords = document.getElementById('carta-contador');
 
 function letterGenerate() {
+  contador = 0;
   paragraph.innerHTML = null;
   const arrayWords = inputInsert.value.split(' ');
   for (let i = 0; i < arrayWords.length; i = i + 1) {
@@ -17,15 +20,18 @@ function letterGenerate() {
     createSpan.classList.add(grupoTamanho[Math.floor(Math.random() * grupoTamanho.length)]);
     createSpan.classList.add(grupoRotacao[Math.floor(Math.random() * grupoRotacao.length)]);
     createSpan.classList.add(grupoInclinacao[Math.floor(Math.random() * grupoInclinacao.length)]);
+    contador += 1;
     paragraph.appendChild(createSpan);
     createSpan.addEventListener('click', aleatoryCreate);
   }
+  countWords.innerHTML = contador;
 }
-btnCreate.addEventListener('click', letterGenerate);
 
-function aleatoryCreate (createSpan) {
+function aleatoryCreate(createSpan) {
   document.createSpan.className =  aleatory[Math.floor(Math.random() * aleatory.length)];
 }
+
+btnCreate.addEventListener('click', letterGenerate);
 
   // function () {
   //   for (let i = 0; i < spanTag.length; i++){
