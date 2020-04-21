1cfa06c4e00edb2a857dd15b046d7e5facc3b626
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-21 11:39:37 -0300

    Tentativa de passar nos requisitos obrigatórios

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
 const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
+const aleatory = ['newspaper', 'magazine1', 'magazine2','medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
 
 function letterGenerate() {
+  paragraph.innerHTML = null;
   const arrayWords = inputInsert.value.split(' ');
   for (let i = 0; i < arrayWords.length; i = i + 1) {
     const createSpan = document.createElement('span');
@@ -16,6 +18,17 @@ function letterGenerate() {
     createSpan.classList.add(grupoRotacao[Math.floor(Math.random() * grupoRotacao.length)]);
     createSpan.classList.add(grupoInclinacao[Math.floor(Math.random() * grupoInclinacao.length)]);
     paragraph.appendChild(createSpan);
+    createSpan.addEventListener('click', aleatoryCreate);
   }
 }
 btnCreate.addEventListener('click', letterGenerate);
+
+function aleatoryCreate (createSpan) {
+  document.createSpan.className =  aleatory[Math.floor(Math.random() * aleatory.length)];
+}
+
+  // function () {
+  //   for (let i = 0; i < spanTag.length; i++){
+  //     spanTag[i].className = spanTag[i][Math.floor(Math.random() * spanTag.length)];
+  //   }
+  // });
