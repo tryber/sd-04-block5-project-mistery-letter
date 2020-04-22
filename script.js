6ef2ca6d13865e6d625dca87f8ec5481dfad9587
danilossousa <danilosantossousa@gmail.com>
2020-04-22 03:11:16 -0300

    v0.3.0.14

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let i;
-const styleCls = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+let j;
 const inpCarTex = document.getElementById('carta-texto');
 const butCriCar = document.getElementById('criar-carta');
 const pCarGer = document.getElementById('carta-gerada');
+const classGroup = {
+  styleGroup: ['newspaper', 'magazine1', 'magazine2'],
+  sizeGroup: ['medium', 'big', 'reallybig'],
+  rotationGroup: ['rotateleft', 'rotateright'],
+  inclinationGroup: ['skewleft', 'skewright'],
+};
 
 function separateWords() {
   const words = inpCarTex.value.split(' ');
@@ -15,6 +21,39 @@ function includeSpan(words) {
     const span = document.createElement('span');
     span.innerText = words[i];
     pCarGer.appendChild(span);
+    console.log(pCarGer)
+  }
+
+  addClass(pCarGer);
+}
+
+function checkClasses(el, cls) {
+  if (el.classList.contains(cls)) {
+    return true;
+  } else {
+    el.classList.add(cls);
+  }
+}
+
+function addClass(parent) {
+  const grpNames = Object.keys(classGroup); // os nomes dos grupos de classes existentes
+  const amountGroupCls = Object.keys(classGroup).length; // a quantidade de grupos de classes existentes
+  let rdmAmtGroup = Math.floor(Math.random() * amountGroupCls) + 1; // a quantidade de grupos q devem ser inseridos em span 
+
+  if (rdmAmtGroup === 1) { 
+    rdmAmtGroup += 1; // garante q tenha pelo menos 2 grupos inseridos em span
+  }
+
+  for (i = 0; i < rdmAmtGroup; i += 1) {
+    for (j = 0; j < parent.children.length; j += 1) {
+      const rdmGroup = Math.floor(Math.random() * amountGroupCls); // o indice do grupo selecionado nesta iteração
+      const nameClsGroup = grpNames[rdmGroup]; // o grupo selecionado nesta iteração
+      const rdmCls = Math.floor(Math.random() * classGroup[nameClsGroup].length); // o indice da classe do grupo selecionado nesta iteração
+
+      if (checkClasses(parent.children[j], classGroup[nameClsGroup][rdmCls])) {
+        break;
+      }
+    }
   }
 }
 
